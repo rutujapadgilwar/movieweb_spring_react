3cc097e809546503e6c5186362af6def46bf967b
Rutuja Padgilwar <rpadgil2@pdx.edu>
2024-09-16 15:26:01 -0700

    Implemented RESTful API for movie listings and reviews using Spring Boot. Added endpoints for fetching movies, reviews, and optimized performance.

---
+++ Backend/src/main/java/com/example/movies/MovieController.java
@@ -0,0 +1,32 @@
+package com.example.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+@CrossOrigin(origins = "http://localhost:3000")
+
+public class MovieController {
+
+    @Autowired
+    private MovieService movieService;
+
+    @GetMapping
+    public ResponseEntity<List<Movie>> getAllMovies(){
+        List<Movie> movies = movieService.allMovies();
+        return new ResponseEntity<>(movies, HttpStatus.OK);
+
+    }
+
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId){
+        return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(imdbId), HttpStatus.OK);
+    }
+}
